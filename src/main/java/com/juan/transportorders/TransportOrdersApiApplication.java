f801ac138ea08a41ee9462e18214045c9a1cc4c2
juanddsuarez <juanddsuareza@gmail.com>
2025-07-08 19:53:26 -0600

    Initial project setup

---
+++ src/main/java/com/juan/transportorders/TransportOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.juan.transportorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportOrdersApiApplication.class, args);
+	}
+
+}
